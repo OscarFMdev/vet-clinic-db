d6f66af9c5ab1b15b6fddb3e50f7af27cfe4fc83
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2022-12-05 17:40:32 -0600

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INTEGER,
+    pet_name varchar(100),
+    data_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg FLOAT,
 );
