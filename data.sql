c4909b2fb405e870a7bf0c711d39f31806755fd9
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2022-12-05 18:08:31 -0600

    Update files for PR

--- data.sql
+++ data.sql
@@ -1,5 +1,3 @@
-/* Populate database with sample data. */
-
 INSERT INTO animals(pet_name, data_of_birth, escape_attempts, neutered, weight_kg) VALUES('Agumon', TO_DATE('02/03/2020', 'MM/DD/YYYY'), 0, TRUE, 10.23);
 INSERT INTO animals(pet_name, data_of_birth, escape_attempts, neutered, weight_kg) VALUES('Gabumon', TO_DATE('11/15/2018', 'MM/DD/YYYY'), 2, TRUE, 8.0);
 INSERT INTO animals(pet_name, data_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', TO_DATE('01/07/2021', 'MM/DD/YYYY'), 1, FALSE, 15.04);
